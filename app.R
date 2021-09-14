0dd51510913dc2d9031af3ce3c7f9b2a6ea27054
Aryuna Ayusheeva <aryuna.ayusheeva.1998@mail.ru>
2021-09-14 23:21:31 +0300

    Update app.R
    Co-Authored-By: Elena Ubogoeva <47380082+ubogoeva@users.noreply.github.com>
    Co-Authored-By: Polyalineli <44047621+Polyalineli@users.noreply.github.com>

--- app.R
+++ app.R
@@ -7,42 +7,75 @@
 #    http://shiny.rstudio.com/
 #
 
+# Setup
+
+# for unified and processed RNA-seq data
+library(recount3)
+# to normalize the RNA-seq data 
+library(DESeq2) 
+# for access to TCGA data
+library(TCGAbiolinks)
+# to look at the data
+library(tidyverse)
+# to visualize the mutation data
+library(maftools)
+# to create heatmaps
+library(ComplexHeatmap)
 library(shiny)
+library(tidyverse)
+library(plotly)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Survival by subtype"),
 
     # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+    selectInput("data", label = h2("Select box"), 
+                choices = list("Survival"="Survival..months." ,"Age of diagnosis"="Age..years.at.diagnosis."), 
+                selected = 1),
+    selectInput("trans", label = h2("Subtype"), 
+                choices = list("Original"="Original.Subtype" ,"Transcriptome"="Transcriptome.Subtype" ), 
+                selected = 1),
+    
+    hr(),
+    fluidRow(column(2, verbatimTextOutput("value"))),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           #plotOutput("distPlot")
+           plotlyOutput("distPlot")
+           
         )
     )
-)
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    tcga_subtype_data <-
+        TCGAquery_subtype(tumor = "GBM")
+    g <-tcga_subtype_data %>%
+        filter(complete.cases(.$Gender))
 
-    output$distPlot <- renderPlot({
+    output$distPlot <- renderPlotly({
         # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        pc <-input$data
+        pc1 <-input$trans
+        g %>%
+            ggplot(aes_(as.name(pc), fill = as.name(pc1))) +
+            geom_density(alpha = 0.4) +
+            facet_wrap(~Gender, scales = "free", ncol = 1) +
+            scale_fill_jco() +
+            theme_linedraw() +
+            theme(axis.title.x = element_blank()) +
+            labs(y = "Density")
+            
+       # ggplotly()
+            
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+        
+     })
 }
 
 # Run the application 
